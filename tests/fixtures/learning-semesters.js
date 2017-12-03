0def0cb9522fd1a6b8f8753403c96d8f3516adc4
Valentyn Hrynevich <valik.grinevich@mev.com>
2017-12-03 11:56:59 +0200

    Trying to fix problem with test in travis CI

--- tests/fixtures/learning-semesters.js
+++ tests/fixtures/learning-semesters.js
@@ -2,8 +2,8 @@ import learningSemestersService from "../../src/admin/services/learning_semester
 
 const learningSemesters = [
     {
-        from_date: '2017-01-01T10:00:00+00:00',
-        to_date: '2017-10-01T10:00:00+00:00',
+        from_date: '2017-01-01T10:00:00+0000',
+        to_date: '2017-10-01T10:00:00+0000',
     }
 ];
 
